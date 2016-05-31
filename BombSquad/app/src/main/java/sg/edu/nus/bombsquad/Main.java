415a9c52f457f39f78ff53e2d3c01fea1670eabc
“marcusngwj” <marcusngwj@msn.com>
2016-05-31 16:54:49 +0800

    Test button added on main page

--- BombSquad/app/src/main/java/sg/edu/nus/bombsquad/Main.java
+++ BombSquad/app/src/main/java/sg/edu/nus/bombsquad/Main.java
@@ -1,7 +1,9 @@
 package sg.edu.nus.bombsquad;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Main extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class Main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button buttonToNext=(Button)findViewById(R.id.buttonMain);
+    }
+
+//    To edit this part
+    public void buttonToNext(){
+        Intent newPage = new Intent(this, RoomType.class);
+        startActivity(newPage);
     }
 }

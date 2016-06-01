93e9e8ca0780fdd47a4ba8b2b9adcaff71cf6b91
“marcusngwj” <marcusngwj@msn.com>
2016-06-01 15:28:48 +0800

    Updated dummy next button on main paeg

--- BombSquad/app/src/main/java/sg/edu/nus/bombsquad/Main.java
+++ BombSquad/app/src/main/java/sg/edu/nus/bombsquad/Main.java
@@ -3,6 +3,7 @@ package sg.edu.nus.bombsquad;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class Main extends AppCompatActivity {
@@ -12,11 +13,15 @@ public class Main extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button buttonToNext=(Button)findViewById(R.id.buttonMain);
+        if (buttonToNext != null) {
+            buttonToNext.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent newPage = new Intent(v.getContext(), LoginPage.class);
+                    startActivity(newPage);
+                }
+            });
+        }
     }
 
-//    To edit this part
-    public void buttonToNext(){
-        Intent newPage = new Intent(this, RoomType.class);
-        startActivity(newPage);
-    }
 }

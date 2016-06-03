53afc203d2ebc097bc3f44bd25edad1e327b02d5
“marcusngwj” <marcusngwj@msn.com>
2016-06-03 13:30:16 +0800

    Updated register system.
    Added database in 000webhost
    Added php files in 000webhost
    
    Error: org.json.JSONException: Value <br><table of type java.lang.String
    cannot be converted to JSONObject

---
+++ BombSquad/app/src/main/java/sg/edu/nus/bombsquad/RegisterRequest.java
@@ -0,0 +1,29 @@
+package sg.edu.nus.bombsquad;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegisterRequest extends StringRequest{
+
+    private static final String REGISTER_REQUEST_URL = "http://orbitalbombsquad.comlu.com/Register.php";
+    private Map<String, String> params;
+
+    public RegisterRequest(String first_name, String last_name, String email, int mobile_no, String username, String password, Response.Listener<String> listener){
+        super(Method.POST, REGISTER_REQUEST_URL, listener, null);
+        params = new HashMap<>();
+        params.put("first_name", first_name);
+        params.put("last_name", last_name);
+        params.put("email", email);
+        params.put("mobile_no", mobile_no + "");
+        params.put("username", username);
+        params.put("password", password);
+    }
+
+    @Override
+    public Map<String, String> getParams(){
+        return params;
+    }
+}

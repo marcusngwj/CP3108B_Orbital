608441efe48343ab5895559822c3f6b178918d37
“marcusngwj” <marcusngwj@msn.com>
2016-06-17 16:49:38 +0800

    App manage to talk to database, but not able to update it with the correct information

--- BombSquad/app/src/main/java/sg/edu/nus/bombsquad/RegisterRequest.java
+++ BombSquad/app/src/main/java/sg/edu/nus/bombsquad/RegisterRequest.java
@@ -1,5 +1,6 @@
 package sg.edu.nus.bombsquad;
 
+
 import com.android.volley.Response;
 import com.android.volley.toolbox.StringRequest;
 
@@ -11,13 +12,13 @@ public class RegisterRequest extends StringRequest{
     private static final String REGISTER_REQUEST_URL = "http://orbitalbombsquad.comlu.com/register.php";
     private Map<String, String> params;
 
-    public RegisterRequest(String first_name, String last_name, String email, int mobile_no, String username, String password, Response.Listener<String> listener){
+    public RegisterRequest(String first_name, String last_name, String email, String mobile_no, String username, String password, Response.Listener<String> listener){
         super(Method.POST, REGISTER_REQUEST_URL, listener, null);
         params = new HashMap<>();
         params.put("first_name", first_name);
         params.put("last_name", last_name);
         params.put("email", email);
-        params.put("mobile_no", mobile_no + "");
+        params.put("mobile_no", mobile_no);
         params.put("username", username);
         params.put("password", password);
     }

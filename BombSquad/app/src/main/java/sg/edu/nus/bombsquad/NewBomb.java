98b53f75489a16e602b80a6785d12d1a52f31a6f
“marcusngwj” <marcusngwj@msn.com>
2016-06-25 03:44:19 +0800

    Updated New bomb Page
    Created:
    - Bomb Name
    - Question Type (Spinner; dropdown-list)
    - Question
    - Answer (Dependent on data type and change accordingly)
    - Time Limit
    - Points Awarded
    - Points Deducted
    - Number of passes
    
    Create "create bomb" button (dummy)
    
    Read data from user

--- BombSquad/app/src/main/java/sg/edu/nus/bombsquad/NewBomb.java
+++ BombSquad/app/src/main/java/sg/edu/nus/bombsquad/NewBomb.java
@@ -2,6 +2,12 @@ package sg.edu.nus.bombsquad;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 
 public class NewBomb extends AppCompatActivity {
@@ -11,6 +17,54 @@ public class NewBomb extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_bomb);
 
-        Spinner spinner = (Spinner) findViewById(R.id.spinnerQuestionType);
+        final EditText etBombName = (EditText) findViewById(R.id.editTextBombName);
+        final Spinner spinner = (Spinner) findViewById(R.id.spinnerQuestionType);
+
+        final EditText etQuestion = (EditText) findViewById(R.id.editTextQuestion);
+
+        final RadioGroup rgMCQ = (RadioGroup) findViewById(R.id.radioGroupMCQ);
+        final EditText etShortLongAnswer = (EditText) findViewById(R.id.editTextShortLongAnswer);
+
+        final EditText etTimeLimit = (EditText) findViewById(R.id.editTextTimeLimit);
+        final EditText etPointsAwarded = (EditText) findViewById(R.id.editTextPointsAwarded);
+        final EditText etPointsDeducted = (EditText) findViewById(R.id.editTextPointsDeducted);
+        final EditText etNumPass = (EditText) findViewById(R.id.editTextNumPass);
+        final Button bCreateBomb = (Button) findViewById(R.id.buttonCreateBomb);
+
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.question_type, android.R.layout.simple_spinner_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String spinnerString = parent.getItemAtPosition(position).toString();
+                if (spinnerString.equals("Multiple Choice")) {
+                    rgMCQ.setVisibility(View.VISIBLE);
+                    etShortLongAnswer.setVisibility(View.GONE);
+                } else {
+                    etShortLongAnswer.setVisibility(View.VISIBLE);
+                    rgMCQ.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        assert bCreateBomb != null;
+        bCreateBomb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String bombName = etBombName.getText().toString();
+                String question = etQuestion.getText().toString();
+                String timeLimit = etTimeLimit.getText().toString();
+                String pointsAwarded = etPointsAwarded.getText().toString();
+                String pointsDeducted = etPointsDeducted.getText().toString();
+                String numPass = etNumPass.getText().toString();
+                String choice = spinner.getSelectedItem().toString();
+            }
+
+        });
     }
 }

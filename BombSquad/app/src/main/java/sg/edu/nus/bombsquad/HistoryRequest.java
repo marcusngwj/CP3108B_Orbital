5f99a6480cb9dd08080cc510ba696e6c234df809
zachylimwl <zachary.lim@hotmail.com>
2016-06-24 22:53:14 +0800

    edit history.php and succesfully pull from database

--- BombSquad/app/src/main/java/sg/edu/nus/bombsquad/HistoryRequest.java
+++ BombSquad/app/src/main/java/sg/edu/nus/bombsquad/HistoryRequest.java
@@ -1,15 +1,26 @@
 package sg.edu.nus.bombsquad;
 
+import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by user on 24/6/2016.
  */
 public class HistoryRequest extends StringRequest {
     private static final String HISTORY_REQUEST_URL = "http://orbitalbombsquad.comlu.com/history.php";
+    private Map<String, String> params;
 
     public HistoryRequest(String user_id, Response.Listener<String> listener) {
-        super(Method.POST, HISTORY_REQUEST_URL, listener, null);
+        super(Request.Method.POST, HISTORY_REQUEST_URL, listener, null);
+        params = new HashMap<>();
+        params.put("user_id", user_id);
+    }
+    @Override
+    public Map<String, String> getParams(){
+        return params;
     }
 }
--- history.php
+++ history.php
@@ -1,28 +1,24 @@
 <?php
     $con = mysqli_connect("mysql12.000webhost.com", "a6020307_squad", "orbital123", "a6020307_squad");
     
-    $username = $_POST["username"];
-    $password = $_POST["password"];
-    
-    $statement = mysqli_prepare($con, "SELECT * FROM user WHERE username = ? AND password = ?");
-    mysqli_stmt_bind_param($statement, "ss", $username, $password);
+	$user_id = $_POST["user_id"];
+
+    $statement = mysqli_prepare($con, "SELECT * FROM Room WHERE user_id = ?");
+    mysqli_stmt_bind_param($statement, "s", $user_id);
     mysqli_stmt_execute($statement);
     
     mysqli_stmt_store_result($statement);
-    mysqli_stmt_bind_result($statement, $userID, $first_name, $last_name, $email, $mobile_no, $username, $password);
+    mysqli_stmt_bind_result($statement, $room_id, $room_name, $user_id, $room_code);
     
     $response = array();
     $response["success"] = false;  
     
     while(mysqli_stmt_fetch($statement)){
         $response["success"] = true;
-		$response["userID"] = $userID;
-		$response["first_name"] = $first_name;
-		$response["last_name"] = $last_name;
-		$response["email"] = $email;
-		$response["mobile_no"] = $mobile_no;
-		$response["username"] = $username;
-		$response["password"] = $password;
+		$response["room_id"] = $room_id;
+		$response["room_name"] = $room_name;
+		$response["user_id"] = $user_id;
+		$response["room_code"] = $room_code;
     }
     
     echo json_encode($response);

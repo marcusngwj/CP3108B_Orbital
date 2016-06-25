8ff1afe76281b7456dd495742b7604eef9a813ba
“marcusngwj” <marcusngwj@msn.com>
2016-06-25 16:25:30 +0800

    Created bombDepo.php

--- bombDepo.php
+++ bombDepo.php
@@ -4,18 +4,19 @@
     $bomb_name = $_POST["bomb_name"];
 	$question_type = $_POST["question_type"];
 	$question = $_POST["question"];
+	$option_one = $_POST["option_one"];
+	$option_two = $_POST["option_two"];
+	$option_three = $_POST["option_three"];
+	$option_four = $_POST["option_four"];
 	$answer = $_POST["answer"];
 	$time_limit = $_POST["time_limit"];
 	$points_awarded = $_POST["points_awarded"];
 	$points_deducted = $_POST["points_deducted"];
 	$num_pass = $_POST["num_pass"];
 	$user_id = $_POST["user_id"];
-	
-	$response = array();
-	$response["success"] = false;
     
-	$result = mysqli_query($con,"INSERT INTO Bomb_Depository (bomb_name, question_type, question, answer, time_limit, points_awarded, points_deducted, num_pass, user_id); 
-      VALUES ('$bomb_name', '$question_type', '$question', '$answer', '$time_limit', '$points_awarded', '$points_deducted', '$num_pass', '$user_id')");
+	$result = mysqli_query($con,"INSERT INTO Bomb_Depository (bomb_name, question_type, question, option_one, option_two, option_three, option_four, answer, time_limit, points_awarded, points_deducted, num_pass, user_id) 
+      VALUES ('$bomb_name', '$question_type', '$question', '$option_one', '$option_two', '$option_three', '$option_four', '$answer', '$time_limit', '$points_awarded', '$points_deducted', '$num_pass', '$user_id')");
 		  
 	if($result == true){
 		echo '{"query_result":"SUCCESS"}';

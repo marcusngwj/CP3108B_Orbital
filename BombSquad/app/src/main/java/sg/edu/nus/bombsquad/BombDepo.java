751db33b2410fe8cc050298b0dbd264ab3ebfaf2
zachylimwl <zachary.lim@hotmail.com>
2016-06-27 01:05:08 +0800

    Attempt to add listener to dynamic checklist

--- BombSquad/app/src/main/java/sg/edu/nus/bombsquad/BombDepo.java
+++ BombSquad/app/src/main/java/sg/edu/nus/bombsquad/BombDepo.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CheckedTextView;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
 import org.json.JSONException;
@@ -34,14 +35,36 @@ public class BombDepo extends AppCompatActivity {
             LinearLayout ll = (LinearLayout) findViewById(R.id.bombDepoScroll);
             LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
             int i = 0;
+            final boolean[] selected = new boolean[100000];
             while (i < bomb.length()) {
                 CheckBox checkbox = (CheckBox)((LayoutInflater)getSystemService(LAYOUT_INFLATER_SERVICE)).inflate(R.layout.checkbox,null);
                 checkbox.setText(bomb.getJSONObject(i+"").getString("bomb_name"));
+                checkbox.setId(Integer.parseInt(bomb.getJSONObject(i+"").getString("question_id")));
                 checkbox.setTextSize(25);
+                checkbox.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        selected[v.getId()] = true;
+                    }
+                });
                 assert ll != null;
                 ll.addView(checkbox, lp);
                 i++;
             }
+
+            ImageButton greenTick = (ImageButton)findViewById(R.id.greenTick);
+            assert greenTick != null;
+            greenTick.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    System.out.println("HELLO");
+                    for(int i = 0; i < 100000; i++) {
+                        if(selected[i]){
+                            System.out.println(i);
+                        }
+                    }
+                }
+            });
         }
         catch (JSONException e) {
             e.printStackTrace();

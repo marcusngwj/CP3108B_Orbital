1c156012811cfb1d780c765bc1023b9457dd9725
zachylimwl <zachary.lim@hotmail.com>
2016-07-18 23:17:23 +0800

    merge

--- BombSquad/app/src/main/java/sg/edu/nus/bombsquad/HostSelection.java
+++ BombSquad/app/src/main/java/sg/edu/nus/bombsquad/HostSelection.java
@@ -3,19 +3,34 @@ package sg.edu.nus.bombsquad;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 public class HostSelection extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.host_selection);
-        display();
+        setContentView(R.layout.activity_host_selection);
+        Intent intent = getIntent();
+        String question_id = intent.getStringExtra("question_id");
+        String room_code = intent.getStringExtra("room_code");
+
+        deployToRandom(room_code, question_id);
+        deployToSelected(room_code, question_id);
     }
-    private void display() {
+
+    private void deployToRandom(String room_code, String question_id) {
+        Button bRandomPlayer = (Button)findViewById(R.id.buttonRandomPlayer);
+        assert bRandomPlayer != null;
+        bRandomPlayer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    private void deployToSelected(String room_code, String question_id) {
+        Button bSelectPlayer = (Button)findViewById(R.id.buttonSelectPlayer);
     }
 }

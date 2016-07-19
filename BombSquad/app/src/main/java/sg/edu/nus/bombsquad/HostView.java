77e9ffae59470a5bf9d9b02d18d708c4cf1710f2
“marcusngwj” <marcusngwj@msn.com>
2016-07-19 23:48:11 +0800

    Successfully retrieve individual data on questions

--- BombSquad/app/src/main/java/sg/edu/nus/bombsquad/HostView.java
+++ BombSquad/app/src/main/java/sg/edu/nus/bombsquad/HostView.java
@@ -1,5 +1,6 @@
 package sg.edu.nus.bombsquad;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.AsyncTask;
@@ -10,6 +11,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.android.volley.RequestQueue;
@@ -35,6 +37,7 @@ import okhttp3.RequestBody;
 
 public class HostView extends AppCompatActivity {
     Global global = Global.getInstance();
+    ProgressDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,28 +53,41 @@ public class HostView extends AppCompatActivity {
         global.setRoom_code(room_code);
         final String[] questionIDArray = global.getQuestion_id();    //Array that contains all the question_ids
 
-        String[][] stringArr = new String[numQuestion][6];  //Initialising String2DArray in global
+        String[][] stringArr = new String[numQuestion][7];  //Initialising String2DArray in global
         global.setData(stringArr);  //Initialising String2DArray in global
 
         System.out.println("ROOM CODE: " + room_code);
         System.out.println("ROOM NAME: " + room_name);
         System.out.println("NUM QUESTION: " + numQuestion);
 
-        TalkToServer myTask = new TalkToServer(); // can add params for a constructor if needed
-        myTask.execute(); // here is where you would pass data to doInBackground()
 
-//        display(global, room_code, room_name, numQuestion, questionIDArray);
-
+        display(room_code, room_name, numQuestion, questionIDArray);
 
     }
 
-    public void getQuestionsData(int numQuestion, String[] questionIDArray){
-        System.out.println("----- INSIDE getQuestionsData -----");
+
+    //To display the entire layout of hostview
+    private void display(String room_code, String room_name, int numQuestion, String[] questionIDArray) {
+        //Display room id
+        TextView tvHostViewBattlefieldRoomName = (TextView) findViewById(R.id.textViewHostViewBattlefieldRoomName);
+        tvHostViewBattlefieldRoomName.setText(room_name + "");
+
+        //Outer Container
+        LinearLayout outerLL = (LinearLayout) findViewById(R.id.linearLayoutHostView);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        lp.setMargins(0, 0, 0, 50);
+
+        getQuestionsData(numQuestion, questionIDArray, outerLL, lp);
+    }
+
+
+    //Main purpose: To retrieve question data from database
+    public void getQuestionsData(int numQuestion, final String[] questionIDArray, final LinearLayout outerLL, final LinearLayout.LayoutParams lp){
         global.setCounter(0);
 
         int i = 0;
         while (i < numQuestion) {
-            System.out.println("qnID: " + questionIDArray[i] + "     (INSIDE GQD)");
+            System.out.println("qnID: " + questionIDArray[i]);
 
 
             Response.Listener<String> responseListener = new Response.Listener<String>() {
@@ -85,22 +101,18 @@ public class HostView extends AppCompatActivity {
                         int i = global.getCounter();
                         tempArr[i][0] = jsonResponse.getJSONObject(0 + "").getString("question_type");
                         tempArr[i][1] = jsonResponse.getJSONObject(0 + "").getString("question");
+                        tempArr[i][2] = jsonResponse.getJSONObject(0 + "").getString("option_one");
+                        tempArr[i][3] = jsonResponse.getJSONObject(0 + "").getString("option_two");
+                        tempArr[i][4] = jsonResponse.getJSONObject(0 + "").getString("option_three");
+                        tempArr[i][5] = jsonResponse.getJSONObject(0 + "").getString("option_four");
+                        tempArr[i][6] = jsonResponse.getJSONObject(0 + "").getString("answer");
 
-
-                        System.out.println("Inside gqd function: " + tempArr[i][1] + "     (INSIDE GQD)");
-                        global.setData(tempArr);
-                        String[][] abc = global.getString2DArr();
-                        System.out.println("global counter: " + i + "     (INSIDE GQD)");
-                        System.out.println("abc: " + abc[global.getCounter()][1] + "     (INSIDE GQD)");
+                        outerLL.addView(createQuestionBox(lp, i, questionIDArray, tempArr));
                         global.setCounter(++i);
-
 
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
-//                    global.setData(tempArr);
-//                    String[][] abc = global.getString2DArr();
-//                    System.out.println("abc: " + abc[0][1]);
                 }
             };
             QuestionAnswerOptionRequest questionAnswerOptionRequest = new QuestionAnswerOptionRequest(questionIDArray[i], responseListener);
@@ -111,188 +123,143 @@ public class HostView extends AppCompatActivity {
         }
     }
 
-    private void display(String room_code, String room_name, int numQuestion, String[] questionIDArray) {
-        System.out.println("----- INSIDE display -----");
 
-        //Display room id
-        TextView tvHostViewBattlefieldRoomName = (TextView) findViewById(R.id.textViewHostViewBattlefieldRoomName);
-        tvHostViewBattlefieldRoomName.setText(room_name + "");
-
-        //Outer Container
-        LinearLayout outerLL = (LinearLayout) findViewById(R.id.linearLayoutHostView);
-        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-        lp.setMargins(0, 0, 0, 50);
-
-        //While loop to display all questions in a selected room
-        int i = 0;
-        while (i < numQuestion) {
-
-            global.setNumber(i);
-
-            //Inner container
-            LinearLayout innerLL = new LinearLayout(this);
-            innerLL.setOrientation(LinearLayout.VERTICAL);
-            innerLL.setBackgroundResource(R.drawable.white_border_transparent_background);
-            innerLL.setPadding(15, 15, 15, 50);
-            innerLL.setId(i);
-            innerLL.setLayoutParams(lp);
+    //To create an individual box that contain details of a particular question
+    private LinearLayout createQuestionBox(LinearLayout.LayoutParams lp, int i, String[] questionIDArray, String[][] createQnBoxArr){
+        //Inner container
+        LinearLayout innerLL = new LinearLayout(this);
+        innerLL.setOrientation(LinearLayout.VERTICAL);
+        innerLL.setBackgroundResource(R.drawable.white_border_transparent_background);
+        innerLL.setPadding(15, 15, 15, 50);
+        innerLL.setId(i);
+        innerLL.setLayoutParams(lp);
 
 
-            //Question - Heading_TextView
-            TextView tvQuestionHeading = new TextView(this);
-            int questionNo = i + 1;
-            tvQuestionHeading.setText("Question " + questionNo);
-            tvQuestionHeading.setTextSize(20);
-            tvQuestionHeading.setTextColor(Color.WHITE);
-            tvQuestionHeading.setPadding(15, 5, 2, 2);
+        //Question - Heading_TextView
+        TextView tvQuestionHeading = new TextView(this);
+        int questionNo = i + 1;
+        tvQuestionHeading.setText("Question " + questionNo);
+        tvQuestionHeading.setTextSize(20);
+        tvQuestionHeading.setTextColor(Color.WHITE);
+        tvQuestionHeading.setPadding(15, 5, 2, 2);
 
-            //Question & Answer_Option - LinearLayout
-            LinearLayout questionLL = new LinearLayout(this);
-            questionLL.setOrientation(LinearLayout.VERTICAL);
-            LinearLayout.LayoutParams qlp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
-            qlp.setMargins(30, 0, 30, 20);
-            questionLL.setLayoutParams(qlp);
+        //Question & Answer_Option - LinearLayout
+        LinearLayout questionLL = new LinearLayout(this);
+        questionLL.setOrientation(LinearLayout.VERTICAL);
+        LinearLayout.LayoutParams qlp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+        qlp.setMargins(30, 0, 30, 20);
+        questionLL.setLayoutParams(qlp);
 
-            //Question - Actual Question_TextView
-            TextView tvQuestion = new TextView(this);
-//            tvQuestion.setText("TEST QN");
-//            tvQuestion.setText(global.getHashMapArrayList().get(i).get("question"));
-//            String[][] qn2DArray = global.getString2DArr();
-//            tvQuestion.setText(qn2DArray[i][1]);
-            System.out.println("INSIDE DISPLAY: " + i + "     (INSIDE display)");
+        //Question - Actual Question_TextView
+        TextView tvQuestion = new TextView(this);
+        String qn = createQnBoxArr[i][1];
+        tvQuestion.setText(qn);
+        tvQuestion.setTextSize(20);
 
 
-            //Answer Option - TextView
-            TextView tvAnswerOption = new TextView(this);
-            tvAnswerOption.setText("change");
-            tvAnswerOption.setTextSize(20);
-
-            questionLL.addView(tvQuestion);
-            questionLL.addView(tvAnswerOption);
-
-
-            //In possession of - TextView
-            TextView tvInPossessionOf = new TextView(this);
-            tvInPossessionOf.setText("In possession of");
-            tvInPossessionOf.setTextSize(20);
-            tvInPossessionOf.setTextColor(Color.WHITE);
-            tvInPossessionOf.setPadding(15, 5, 2, 2);
-
-            //In possession of - EditText
-            LinearLayout.LayoutParams etIPOLL = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
-            etIPOLL.setMargins(30, 0, 30, 20);
-            EditText etIPO = new EditText(this);
-            etIPO.setPadding(15, 15, 12, 12);
-            etIPO.setWidth(30);
-            etIPO.setBackgroundResource(R.drawable.white_bg_black_border);
-            etIPO.setLayoutParams(etIPOLL);
-
-            //Time Left - TextView
-            TextView tvTimeLeft = new TextView(this);
-            tvTimeLeft.setText("Time Left");
-            tvTimeLeft.setTextSize(20);
-            tvTimeLeft.setTextColor(Color.WHITE);
-            tvTimeLeft.setPadding(15, 5, 2, 2);
-
-            //Time Left - EditText
-            LinearLayout.LayoutParams etTimeLeftLL = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
-            etTimeLeftLL.setMargins(30, 0, 30, 35);
-            EditText etTimeLeft = new EditText(this);
-            etTimeLeft.setPadding(15, 15, 12, 12);
-            etTimeLeft.setWidth(30);
-            etTimeLeft.setBackgroundResource(R.drawable.white_bg_black_border);
-            etTimeLeft.setLayoutParams(etTimeLeftLL);
-
-            //LinearLayout for defuse and detonate
-            LinearLayout defuseDetonateLL = new LinearLayout(this);
-            defuseDetonateLL.setOrientation(LinearLayout.HORIZONTAL);
-            defuseDetonateLL.setGravity(Gravity.CENTER);
-            LinearLayout.LayoutParams ddlp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-            ddlp.setMargins(0, 0, 40, 0);
-
-            //Deploy - Button (So host will see all the question in the room and then choose to deploy it from here)
-            final Button bDeploy = new Button(this);
-            bDeploy.setBackgroundResource(R.drawable.white_bg_black_border);
-            bDeploy.setText("Deploy");
-            bDeploy.setTag(questionIDArray[i]);
-            bDeploy.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(HostView.this, HostSelection.class);
-                    String question_id = (String) bDeploy.getTag();
-                    System.out.println("question_id: " + question_id);
-                    intent.putExtra("question_id", question_id);
-                    intent.putExtra("room_code", global.getRoom_code());
-                    HostView.this.startActivity(intent);
-                }
-            });
-
-            //Defuse - Button
-            Button bDefuse = new Button(this);
-            bDefuse.setBackgroundResource(R.drawable.green_bg_black_border);
-            bDefuse.setText("Defuse");
-            bDefuse.setLayoutParams(ddlp);
-
-            //Detonate - Button
-            Button bDetonate = new Button(this);
-            bDetonate.setBackgroundResource(R.drawable.red_bg_black_border);
-            bDetonate.setText("Detonate");
-
-            defuseDetonateLL.addView(bDeploy);
-            defuseDetonateLL.addView(bDefuse);
-            defuseDetonateLL.addView(bDetonate);
-
-            //Format of front-end in order
-            innerLL.addView(tvQuestionHeading);
-            innerLL.addView(questionLL);
-            innerLL.addView(tvInPossessionOf);
-            innerLL.addView(etIPO);
-            innerLL.addView(tvTimeLeft);
-            innerLL.addView(etTimeLeft);
-            innerLL.addView(defuseDetonateLL);
-
-            outerLL.addView(innerLL, lp);
-
-            i++;
+        //Answer Option - TextView
+        TextView tvAnswerOption = new TextView(this);
+        String result = "";
+        String qnType = createQnBoxArr[i][0];
+        if(qnType.equals("Multiple Choice")){
+            for(int j=2; j<6; j++){
+                result += createQnBoxArr[i][j] + "\n";
+            }
         }
+        else{
+            result = createQnBoxArr[i][6];
+        }
+        tvAnswerOption.setText(result);
+        tvAnswerOption.setTextSize(20);
+
+        questionLL.addView(tvQuestion);
+        questionLL.addView(tvAnswerOption);
+
+
+        //In possession of - TextView
+        TextView tvInPossessionOf = new TextView(this);
+        tvInPossessionOf.setText("In possession of");
+        tvInPossessionOf.setTextSize(20);
+        tvInPossessionOf.setTextColor(Color.WHITE);
+        tvInPossessionOf.setPadding(15, 5, 2, 2);
+
+        //In possession of - EditText
+        LinearLayout.LayoutParams etIPOLL = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+        etIPOLL.setMargins(30, 0, 30, 20);
+        EditText etIPO = new EditText(this);
+        etIPO.setPadding(15, 15, 12, 12);
+        etIPO.setWidth(30);
+        etIPO.setBackgroundResource(R.drawable.white_bg_black_border);
+        etIPO.setLayoutParams(etIPOLL);
+
+        //Time Left - TextView
+        TextView tvTimeLeft = new TextView(this);
+        tvTimeLeft.setText("Time Left");
+        tvTimeLeft.setTextSize(20);
+        tvTimeLeft.setTextColor(Color.WHITE);
+        tvTimeLeft.setPadding(15, 5, 2, 2);
+
+        //Time Left - EditText
+        LinearLayout.LayoutParams etTimeLeftLL = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+        etTimeLeftLL.setMargins(30, 0, 30, 35);
+        EditText etTimeLeft = new EditText(this);
+        etTimeLeft.setPadding(15, 15, 12, 12);
+        etTimeLeft.setWidth(30);
+        etTimeLeft.setBackgroundResource(R.drawable.white_bg_black_border);
+        etTimeLeft.setLayoutParams(etTimeLeftLL);
+
+        //LinearLayout for defuse and detonate
+        LinearLayout defuseDetonateLL = new LinearLayout(this);
+        defuseDetonateLL.setOrientation(LinearLayout.HORIZONTAL);
+        defuseDetonateLL.setGravity(Gravity.CENTER);
+        LinearLayout.LayoutParams ddlp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        ddlp.setMargins(0, 0, 40, 0);
+
+        //Deploy - Button (So host will see all the question in the room and then choose to deploy it from here)
+        final Button bDeploy = new Button(this);
+        bDeploy.setBackgroundResource(R.drawable.white_bg_black_border);
+        bDeploy.setText("Deploy");
+        bDeploy.setTag(questionIDArray[i]);
+        bDeploy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HostView.this, HostSelection.class);
+                String question_id = (String) bDeploy.getTag();
+                System.out.println("question_id: " + question_id);
+                intent.putExtra("question_id", question_id);
+                intent.putExtra("room_code", global.getRoom_code());
+                HostView.this.startActivity(intent);
+            }
+        });
+        bDeploy.setLayoutParams(ddlp);
+
+        //Defuse - Button
+        Button bDefuse = new Button(this);
+        bDefuse.setBackgroundResource(R.drawable.green_bg_black_border);
+        bDefuse.setText("Defuse");
+        bDefuse.setLayoutParams(ddlp);
+
+        //Detonate - Button
+        Button bDetonate = new Button(this);
+        bDetonate.setBackgroundResource(R.drawable.red_bg_black_border);
+        bDetonate.setText("Detonate");
+
+        defuseDetonateLL.addView(bDeploy);
+        defuseDetonateLL.addView(bDefuse);
+        defuseDetonateLL.addView(bDetonate);
+
+        //Format of front-end in order
+        innerLL.addView(tvQuestionHeading);
+        innerLL.addView(questionLL);
+        innerLL.addView(tvInPossessionOf);
+        innerLL.addView(etIPO);
+        innerLL.addView(tvTimeLeft);
+        innerLL.addView(etTimeLeft);
+        innerLL.addView(defuseDetonateLL);
+
+        return innerLL;
     }
-
-
-    class TalkToServer extends AsyncTask<Void, Void, Void> {
-
-        final Intent intent = getIntent();
-        final String room_code = intent.getStringExtra("room_code");
-        final String room_name = intent.getStringExtra("room_name");
-        final int numQuestion = Integer.valueOf(intent.getStringExtra("numQuestion"));
-        final Global global = Global.getInstance();
-        final String[] questionIDArray = global.getQuestion_id();    //Array that contains all the question_ids
-
-        @Override
-        protected void onPreExecute() {
-            global.setRoom_code(room_code);
-            String[][] stringArr = new String[numQuestion][6];  //Initialising String2DArray in global
-            global.setData(stringArr);  //Initialising String2DArray in global
-        }
-
-        @Override
-        protected Void doInBackground(Void... params) {
-            getQuestionsData(numQuestion, questionIDArray);
-            String[][] tempArr = global.getString2DArr();
-            System.out.println("HELPPPPP: " + tempArr[global.getCounter()][1]);
-
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void result) {
-            String[][] tempArr = global.getString2DArr();
-            System.out.println("WHATTTTT: " + tempArr[0][1]);
-
-            display(room_code, room_name, numQuestion, questionIDArray);
-
-        }
-    }
-
+    
 
 }
 

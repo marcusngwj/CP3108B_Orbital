f1850b5e42ab2571790b3e70d02700185218d8ae
“marcusngwj” <marcusngwj@msn.com>
2016-07-20 08:24:48 +0800

    Added user_id variable and setter getter methods

--- BombSquad/app/src/main/java/sg/edu/nus/bombsquad/Global.java
+++ BombSquad/app/src/main/java/sg/edu/nus/bombsquad/Global.java
@@ -22,6 +22,7 @@ public class Global {
 
     
     //Variables with defined purpose
+    private String user_id;
     private String room_name;
     private String room_code;
     private String room_status;
@@ -31,6 +32,7 @@ public class Global {
     private String[] player_id = new String[100000];
     private String[] player_list;
     private boolean booleanAccess;
+
 
 
 
@@ -47,19 +49,29 @@ public class Global {
 
     public void setData(int[] arr) { intArr = arr; }
 
-    public void setRoomStatus(String string) {
-        room_status = string;
-    }
-
-    public void setQuestion_id(String[] arr) { question_id = arr; }
-
-    public void setRoomCode(String string) { room_code = string; }
-
     public void setNumber(int integer) { number = integer; }
 
     public void setCounter(int newCount) { counter = newCount; }
 
     public void setBooleanVar(boolean var) { booleanVar = var; }
+
+    public void setUserId(String id) { user_id = id; }
+
+    public void setRoomName(String name) {
+        room_name = name;
+    }
+
+    public void setRoomCode(String string) { room_code = string; }
+
+    public void setRoomStatus(String string) {
+        room_status = string;
+    }
+
+    public void setNumQuestion(int num) {
+        num_question = num;
+    }
+
+    public void setQuestion_id(String[] arr) { question_id = arr; }
 
     public void setPlayerName (String name) {
         player_name = name;
@@ -77,13 +89,9 @@ public class Global {
         booleanAccess = bool;
     }
 
-    public void setRoomName(String name) {
-        room_name = name;
-    }
 
-    public void setNumQuestion(int num) {
-        num_question = num;
-    }
+
+
 
 
 
@@ -102,19 +110,29 @@ public class Global {
 
     public int[] getIntArr() { return intArr; }
 
-    public String getRoomStatus() {
-        return room_status;
-    }
-
-    public String[] getQuestion_id() { return question_id; }
-
-    public String getRoomCode() { return room_code; }
-
     public int getNumber() { return number; }
 
     public int getCounter() { return counter; }
 
     public boolean getBooleanVar() { return booleanVar; }
+
+    public String getUserId() { return user_id; }
+
+    public String getRoomName() {
+        return room_name;
+    }
+
+    public String getRoomCode() { return room_code; }
+
+    public String getRoomStatus() {
+        return room_status;
+    }
+
+    public int getNumQuestion() {
+        return num_question;
+    }
+
+    public String[] getQuestion_id() { return question_id; }
 
     public String getPlayerName() {
         return player_name;
@@ -132,13 +150,11 @@ public class Global {
         return booleanAccess;
     }
 
-    public String getRoomName() {
-        return room_name;
-    }
 
-    public int getNumQuestion() {
-        return num_question;
-    }
+
+
+
+
 
 
 

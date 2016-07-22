c3f9bea2e8e648c427bcc041abb273f036f78a5d
“marcusngwj” <marcusngwj@msn.com>
2016-07-23 00:49:14 +0800

    Removed unneccessary lines of codes

--- BombSquad/app/src/main/java/sg/edu/nus/bombsquad/RoomDetail.java
+++ BombSquad/app/src/main/java/sg/edu/nus/bombsquad/RoomDetail.java
@@ -3,21 +3,14 @@ package sg.edu.nus.bombsquad;
 public class RoomDetail {
     /*---------- Variables ----------*/
     String room_id;
-    String room_name;
-    String user_id;
-    String room_code;
     String question_id;
     String deploy_status;
     String time_left;
     String player_id;   //Player holding onto the bomb
 
     /*---------- Constructor ----------*/
-    public RoomDetail(String room_id, String room_name, String user_id, String room_code, String question_id, String deploy_status,
-                      String time_left, String player_id) {
+    public RoomDetail(String room_id, String question_id, String deploy_status, String time_left, String player_id) {
         this.room_id = room_id;
-        this.room_name = room_name;
-        this.user_id = user_id;
-        this.room_code = room_code;
         this.question_id = question_id;
         this.deploy_status = deploy_status;
         this.time_left = time_left;
@@ -26,21 +19,8 @@ public class RoomDetail {
 
 
 
-
-
-
-
-
-
-
     /*---------- Setter ----------*/
     public void setRoom_id(String room_id) { this.room_id = room_id; }
-
-    public void setRoom_name(String room_name) { this.room_name = room_name; }
-
-    public void setUser_id(String user_id) { this.user_id = user_id; }
-
-    public void setRoom_code(String room_code) { this.room_code = room_code; }
 
     public void setQuestion_id(String question_id) { this.question_id = question_id; }
 
@@ -55,20 +35,9 @@ public class RoomDetail {
 
 
 
-
-
-
-
-
     /*---------- Getter ----------*/
 
     public String getRoom_id() { return room_id; }
-
-    public String getRoom_name() { return room_name; }
-
-    public String getUser_id() { return user_id; }
-
-    public String getRoom_code() { return room_code; }
 
     public String getQuestion_id() { return question_id; }
 

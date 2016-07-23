03c64e1a79abed7ff26b1d84b1475694b3e8242a
“marcusngwj” <marcusngwj@msn.com>
2016-07-24 02:11:41 +0800

    update getPlayersInGame

--- BombSquad/app/src/main/java/sg/edu/nus/bombsquad/RoomBank.java
+++ BombSquad/app/src/main/java/sg/edu/nus/bombsquad/RoomBank.java
@@ -1,9 +1,21 @@
 package sg.edu.nus.bombsquad;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
 
 //This class serves as a container to contain information from a same room_code
 //  and the questions tied to that room_code
+//In short, another way of calling Room Bank, is Game
 public class RoomBank {
     String room_name;
     String room_code;
@@ -11,6 +23,7 @@ public class RoomBank {
     ArrayList<RoomDetail> roomDetailList;
     ArrayList<QuestionDetail> questionDetailList;
     ArrayList<String> questionIDList;
+    ArrayList<String> playersInGameList;
 
     /*---------- Constructor ----------*/
     public RoomBank(String room_name, String room_code) {
@@ -31,4 +44,43 @@ public class RoomBank {
     public ArrayList<RoomDetail> getRoomDetailList() { return roomDetailList; }
     public ArrayList<QuestionDetail> getQuestionDetailList() { return questionDetailList; }
     public ArrayList<String> getQuestionIDList() { return questionIDList; }
+
+    public ArrayList<String> getPlayersInGameList() {
+        playersInGameList = new ArrayList<String>();
+
+        OkHttpClient client = new OkHttpClient();
+        RequestBody postData = new FormBody.Builder()
+                .add("room_code", room_code)
+                .build();
+        Request request = new Request.Builder().url("http://orbitalbombsquad.x10host.com/playersInGame.php").post(postData).build();
+
+        client.newCall(request)
+                .enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+                        System.out.println("FAIL");
+                    }
+
+                    @Override
+                    public void onResponse(Call call, okhttp3.Response response) throws IOException {
+                        try {
+                            JSONObject result = new JSONObject(response.body().string());
+
+                            int numPlayers = Integer.valueOf(result.getJSONObject(0+"").getString("numRow"));
+                            System.out.println("numPlayers: " + numPlayers);
+
+                            for(int i=0; i<numPlayers; i++){
+                                String player_id = result.getJSONObject(i+"").getString("player");
+                                System.out.println("player_id: " + player_id);
+                                playersInGameList.add(player_id);
+                            }
+                        } catch (JSONException e) {
+                            /*e.printStackTrace();*/
+                        }
+
+                    }
+                });
+
+        return playersInGameList;
+    }
 }

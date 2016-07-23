4a224c79d60dfff71dc08e8683628919c21e7b8e
“marcusngwj” <marcusngwj@msn.com>
2016-07-24 02:12:40 +0800

    Display list of available players in game who can pass bomb to
    Still debugging, list doesnt display any players

--- BombSquad/app/src/main/java/sg/edu/nus/bombsquad/BombPassPlayerSelection.java
+++ BombSquad/app/src/main/java/sg/edu/nus/bombsquad/BombPassPlayerSelection.java
@@ -1,12 +1,22 @@
 package sg.edu.nus.bombsquad;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -16,8 +26,11 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 
 public class BombPassPlayerSelection extends AppCompatActivity {
+    final ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
     Global global = Global.getInstance();
     RoomBank roomBank = global.getRoomBank();
+    LinearLayout bombPassPlayerSelectionLL;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,32 +41,66 @@ public class BombPassPlayerSelection extends AppCompatActivity {
         System.out.println("Activity Name: BombPassPlayerSelection");
 
 //        display();
+
+        bombPassPlayerSelectionLL = (LinearLayout)findViewById(R.id.LinearLayoutBombPassPlayerSelection);
+
+        scheduler.scheduleAtFixedRate(new Runnable() {
+            public void run() {
+                new Background().execute();
+            }
+        }, 0, 2500, TimeUnit.MILLISECONDS);
+
     }
 
-    public void display(){
-        OkHttpClient client = new OkHttpClient();
-        RequestBody postData = new FormBody.Builder()
-                .add("room_code", roomBank.getRoomCode())
-                .build();
-        Request request = new Request.Builder().url("http://orbitalbombsquad.x10host.com/removePlayerFromGame.php").post(postData).build();
+//    private void display(){
 
-        client.newCall(request)
-                .enqueue(new Callback() {
-                    @Override
-                    public void onFailure(Call call, IOException e) {
-                        System.out.println("FAIL");
-                    }
 
-                    @Override
-                    public void onResponse(Call call, okhttp3.Response response) throws IOException {
-                        try {
-                            JSONObject result = new JSONObject(response.body().string());
-                            System.out.println(result);
-                        } catch (JSONException e) {
-                            /*e.printStackTrace();*/
-                        }
 
-                    }
-                });
+        /*scheduler.scheduleAtFixedRate(new Runnable() {
+            public void run() {
+
+            }
+        }, 0, 2500, TimeUnit.MILLISECONDS);*/
+
+//    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        scheduler.shutdown();
     }
+
+    class Background extends AsyncTask<Void, Void, Void> {
+        ArrayList<String> playersInGameList;
+
+        protected void onPreExecute(Void pre) {
+        }
+
+        protected Void doInBackground(Void... param) {
+            playersInGameList = roomBank.getPlayersInGameList();
+            return null;
+        }
+
+        protected void onPostExecute(Void update) {
+            if(playersInGameList!=null) {
+                int numPlayers = playersInGameList.size();
+
+                System.out.println("NUMPLAYERS: " + numPlayers);
+
+
+                for (int i = 0; i < numPlayers; i++) {
+                    String player_id = playersInGameList.get(i);
+
+                    Button bPlayer = new Button(BombPassPlayerSelection.this);
+                    bPlayer.setText(player_id);
+
+                    bombPassPlayerSelectionLL.addView(bPlayer);
+                }
+            }
+
+
+        }
+    }
+
+
 }

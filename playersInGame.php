ca7b22b4a3a2e062ebd8bebb9c3ba944db676165
“marcusngwj” <marcusngwj@msn.com>
2016-07-24 02:11:22 +0800

    php call to server to get players in game

---
+++ playersInGame.php
@@ -0,0 +1,27 @@
+<?php
+    $con = mysqli_connect("orbitalbombsquad.x10host.com", "orbital2", "h3llo world", "orbital2_bombsquad");
+    
+    $room_code = $_POST["room_code"];
+    
+    $statement = mysqli_prepare($con, "SELECT * FROM Game WHERE room_code = ?");
+    mysqli_stmt_bind_param($statement, "s", $room_code);
+    mysqli_stmt_execute($statement);
+    
+    mysqli_stmt_store_result($statement);
+    mysqli_stmt_bind_result($statement, $game_id, $host, $room_status, $room_code, $player);
+	
+	$num_rows = mysqli_stmt_num_rows($statement);
+    
+    $response = array(array(), array());
+    $response["success"] = false;  
+    
+    $i = 0;
+    while(mysqli_stmt_fetch($statement)){
+        $response[$i]["success"] = true;
+		$response[$i]["player"] = $player;
+		$response[$i]["numRow"] = $num_rows;
+		$i++;
+    }
+    mysqli_close($con);
+	echo json_encode($response);
+?>

545f7ff0d08c588fb6fd4b7109e17385f4343431
“marcusngwj” <marcusngwj@msn.com>
2016-07-23 18:50:44 +0800

    Choose from a list of available players

---
+++ BombSquad/app/src/main/java/sg/edu/nus/bombsquad/BombPassPlayerSelection.java
@@ -0,0 +1,16 @@
+package sg.edu.nus.bombsquad;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BombPassPlayerSelection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bomb_pass_player_selection);
+
+        //To show on Android Monitor onCreate
+        System.out.println("Activity Name: BombPassPlayerSelection");
+    }
+}

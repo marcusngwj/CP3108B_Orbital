166ebb7a4079f5fb797c33713488ee6fb7c5dae6
“marcusngwj” <marcusngwj@msn.com>
2016-07-23 18:50:19 +0800

    Added 2 modes: Pass to random / Pass to Selected

---
+++ BombSquad/app/src/main/java/sg/edu/nus/bombsquad/BombPassSelectionType.java
@@ -0,0 +1,43 @@
+package sg.edu.nus.bombsquad;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class BombPassSelectionType extends AppCompatActivity {
+    Global global = Global.getInstance();
+    RoomBank roomBank = global.getRoomBank();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bomb_pass_selection_type);
+
+        //To show on Android Monitor onCreate
+        System.out.println("Activity Name: BombPassSelectionType");
+
+        passToRandom();
+        passToSelected();
+    }
+
+    //Pass bomb to random player
+    private void passToRandom(){
+        Button bRandomPlayer = (Button)findViewById(R.id.buttonRandomPlayerBPST);
+    }
+
+
+    //Pass bomb to selected player
+    private void passToSelected(){
+        Button bSelectPlayer = (Button)findViewById(R.id.buttonSelectPlayerBPST);
+        bSelectPlayer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentPTS = new Intent(BombPassSelectionType.this, BombPassPlayerSelection.class);
+                startActivity(intentPTS);
+            }
+        });
+
+    }
+}

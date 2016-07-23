7e68431fc1040ede75b0a70d6b6561bc1cc4b0a2
“marcusngwj” <marcusngwj@msn.com>
2016-07-23 23:47:25 +0800

    Added new java file

--- BombSquad/app/src/main/java/sg/edu/nus/bombsquad/BombPassPlayerSelection.java
+++ BombSquad/app/src/main/java/sg/edu/nus/bombsquad/BombPassPlayerSelection.java
@@ -3,6 +3,18 @@ package sg.edu.nus.bombsquad;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+
 public class BombPassPlayerSelection extends AppCompatActivity {
     Global global = Global.getInstance();
     RoomBank roomBank = global.getRoomBank();
@@ -15,10 +27,33 @@ public class BombPassPlayerSelection extends AppCompatActivity {
         //To show on Android Monitor onCreate
         System.out.println("Activity Name: BombPassPlayerSelection");
 
-        display();
+//        display();
     }
 
     public void display(){
+        OkHttpClient client = new OkHttpClient();
+        RequestBody postData = new FormBody.Builder()
+                .add("room_code", roomBank.getRoomCode())
+                .build();
+        Request request = new Request.Builder().url("http://orbitalbombsquad.x10host.com/removePlayerFromGame.php").post(postData).build();
 
+        client.newCall(request)
+                .enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+                        System.out.println("FAIL");
+                    }
+
+                    @Override
+                    public void onResponse(Call call, okhttp3.Response response) throws IOException {
+                        try {
+                            JSONObject result = new JSONObject(response.body().string());
+                            System.out.println(result);
+                        } catch (JSONException e) {
+                            /*e.printStackTrace();*/
+                        }
+
+                    }
+                });
     }
 }
